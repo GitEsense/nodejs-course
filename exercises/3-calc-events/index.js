30f350fe1e5acc81ef054f72299be520cf770631
GitEsense <klg-hunter@mail.ru>
2024-02-03 13:26:46 +0200

    Как работает Node.js - Домашнее задание (Step - 4) - v.2.2
    Переписан emitter

--- exercises/3-calc-events/index.js
+++ exercises/3-calc-events/index.js
@@ -1,23 +1,21 @@
 const myEmitter = new myEmitter();
-const actions = [
-    { title: 'add', action: (a, b) => a + b },
-    { title: 'multiple', action: (a, b) => a * b },
-    {
-        title: 'division',
-        action: (a, b) => {
-            if (b === 0) {
-                return 'Нельзя делить на ноль';
-            }
-            return a / b;
-        },
-    },
-    { title: 'substraction', action: (a, b) => a - b },
-];
 
-for (const item of actions) {
-    myEmitter.on(item.title, (a, b) => {
-        myEmitter.emit('result', item.action(a, b));
-    });
-}
-myEmitter.emit('add', 5, 6);
-myEmitter.on('result', (res) => console.log(res));
+const { add } = require("../3-calc/add.js");
+const { multiple } = require("../3-calc/multiple.js");
+const { division } = require("../3-calc/division.js");
+const { substraction } = require("../3-calc/substraction.js");
+
+const actions = {
+  add: add,
+  multiple: multiple,
+  division: division,
+  substraction: substraction,
+};
+
+Object.entries(actions).map(([key, action]) => {
+  myEmitter.on(key, (a, b) => {
+    myEmitter.emit("result", action(a, b));
+  });
+});
+myEmitter.emit("add", 5, 6);
+myEmitter.on("result", (res) => console.log(res));
