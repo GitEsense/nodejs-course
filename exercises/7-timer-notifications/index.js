59870a2fb3361a410e731d1d7acc1356ac322803
GitEsense <klg-hunter@mail.ru>
2024-02-03 16:43:20 +0200

    Node Package Manager - Домашнее задание (Step - 7.7) - v.2.1
    Добавлены обработчики, переделано на объекты

--- exercises/7-timer-notifications/index.js
+++ exercises/7-timer-notifications/index.js
@@ -1,32 +1,48 @@
 import boom from "./scripts/caboom.js";
 import notifier from "node-notifier";
-const timeString = process.argv.at(-1);
-
 const template = {
   h: 1000 * 60 * 60,
   m: 1000 * 60,
   s: 1000,
 };
+const errorMessage =
+  new Error(`Неверно заданы параметр запуска, используйте формат 
+    h - hour
+    m - minute
+    s - second
+    Пример: "22h 10m 33s"
+    `);
 
-function handleTimeString(str) {
+function handleTimeString(str = "") {
   const values = str.split(" ");
   return values.reduce((total, item) => {
-    const num = Number.parseInt(item);
+    const num = parseInt(item);
     const char = item.slice(-1);
     const ms = template[char];
     total += num * ms;
     return total;
   }, 0);
 }
+
 const sendNotification = (options) => {
   notifier.notify(options);
 };
+
 function main() {
-  const time = handleTimeString(timeString);
-  const options = {
-    title: "My notification",
-    message: "Boom!",
-  };
-  boom(time, () => sendNotification(options));
+  try {
+    const timeString = process.argv[2];
+    const time = handleTimeString(timeString);
+    if (!time) {
+      throw errorMessage;
+    }
+    const options = {
+      title: "My notification",
+      message: "Boom!",
+    };
+    boom(time, () => sendNotification(options));
+  } catch (e) {
+    console.error(e.message);
+  }
 }
+
 main();
