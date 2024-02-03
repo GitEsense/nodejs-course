43c29c2fcbcbfce2d1f4bcb62014b5e09b9238c3
GitEsense <klg-hunter@mail.ru>
2024-02-03 16:38:41 +0200

    Как работает Node.js - Домашнее задание (Step - 4) - v.2.4
    Добавлена обработка

--- exercises/4-timer/caboom.mjs
+++ exercises/4-timer/caboom.mjs
@@ -1,14 +1,6 @@
 import timeFormatter from "./time-formatter.mjs";
 
 export default function boomTime(time, callback) {
-  if (!time) {
-    return console.log(`Неверно заданы параметр запуска, используйте формат 
-    h - hour
-    m - minute
-    s - second
-    Пример: "22h 10m 33s"
-    `);
-  }
   console.log(`Запускаем обратный отсчёт!`);
   console.log(`До взрыва осталось: ${timeFormatter(Math.floor(time))}`);
   const interval = setInterval(() => {
--- exercises/4-timer/index.mjs
+++ exercises/4-timer/index.mjs
@@ -1,17 +1,22 @@
 import boom from "./caboom.mjs";
 
-const timeString = process.argv.at(-1);
-
 const template = {
   h: 1000 * 60 * 60,
   m: 1000 * 60,
   s: 1000,
 };
+const errorMessage =
+  new Error(`Неверно заданы параметр запуска, используйте формат 
+    h - hour
+    m - minute
+    s - second
+    Пример: "22h 10m 33s"
+    `);
 
-function handleTimeString(str) {
+function handleTimeString(str = "") {
   const values = str.split(" ");
   return values.reduce((total, item) => {
-    const num = Number.parseInt(item);
+    const num = parseInt(item);
     const char = item.slice(-1);
     const ms = template[char];
     total += num * ms;
@@ -20,8 +25,16 @@ function handleTimeString(str) {
 }
 
 function main() {
-  const time = handleTimeString(timeString);
-  boom(time, () => console.log("Boom!"));
+  try {
+    const timeString = process.argv[2];
+    const time = handleTimeString(timeString);
+    if (!time) {
+      throw errorMessage;
+    }
+    boom(time, () => console.log("Boom!"));
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
 main();
